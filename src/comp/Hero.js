3c4996a091113c39a4333a571e06518770b42350
Parit-R <parit2004sg@gmail.com>
2025-08-03 20:38:16 +0700

    Add files via upload

---
+++ src/comp/Hero.js
@@ -0,0 +1,12 @@
+import "./HeroStyles.css"
+function Hero (){
+    return(
+        <>
+            <div className="hero">
+                <img alt="Himg" src="https://cdn.pixabay.com/photo/2021/08/01/17/31/path-6514885_1280.jpg"></img>
+            </div>
+        </>
+    )
+}
+
+export default Hero;
---
+++ src/routes/Contract.js
@@ -0,0 +1,9 @@
+function Contract (){
+    return(
+        <>
+        <h1>This is Con</h1>
+        </>
+    )
+}
+
+export default Contract;
